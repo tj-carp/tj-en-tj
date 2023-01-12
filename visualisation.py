eec93515cbc3da15fd32a9b659b5912a9042df4e
BillBelieve <billbelieveiot@gmail.com>
2023-01-12 12:14:23 +0100

    visualisatie van de stations gereed, volgende stap is verbindingen toevoegen.

--- visualisation.py
+++ visualisation.py
@@ -5,8 +5,10 @@ df = pd.read_csv("StationsHolland.csv")
 
 print(df.head())
 
-plt.scatter(x=df['x'], y=df['y'], s=100, c='yellow', edgecolors='blue', linewidths=1)
-[plt.text( x=row['x'], y=row['y'], s=row['station']) for k, row in df.iterrows()]
+
+
+plt.scatter(x=df['x'], y=df['y'], s=10, c='yellow', edgecolors='blue', linewidths=1)
+[plt.text( x=row['x'] + 0.005, y=row['y'], s=row['station'], fontsize=7) for k, row in df.iterrows()]
 plt.ylim(51.75, 53)
 plt.title('Visualisation of train stations')
 plt.xlabel('latitude')
