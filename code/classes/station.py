508f6977a8976d11336ad09684018c4ec0553bb9
Jvdm-0 <jacovandermeij@outlook.com>
2023-01-13 16:57:51 +0100

    station uitcommenten

--- code/classes/station.py
+++ code/classes/station.py
@@ -2,8 +2,8 @@ class Station:
     
     def __init__(self, name: str):
         self._name = name
-        self._adjacents = {i.station1 : i.distance for i in df.itertuples() if i.station2 == name}
-        for i in df.itertuples():
-            if i.station1 == name:
-                self._adjacents[i.station2] = i.distance
+        self._adjacents = {}#{i.station1 : i.distance for i in df.itertuples() if i.station2 == name}
+        # for i in df.itertuples():
+        #     if i.station1 == name:
+        #         self._adjacents[i.station2] = i.distance
             
