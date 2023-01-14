4bf979b3b668fd5569e5db887d3416883c92f093
TJ <tessajleeuwerik@gmail.com>
2023-01-14 13:19:32 +0100

    deleted unused files

--- code/classes/connection.py
+++
@@ -1,6 +0,0 @@
-class Connection:
-    
-    def __init__(self, station1: 'Station', station2: 'Station', distance: int):
-        self._station1 = station1
-        self._station2 = station2
-        self._distance = distance
