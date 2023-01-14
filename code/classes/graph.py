88dc184e58c4be257262c009f6e01e591835ab22
TJ <tessajleeuwerik@gmail.com>
2023-01-14 11:38:00 +0100

    adjusted main.py made graph.py

--- code/classes/graph.py
+++ code/classes/graph.py
@@ -4,17 +4,17 @@ from .station import Station
 
 
 class Graph():
-    def __init__(self, source_file):
-        self.stations = self.load_stations(source_file)
-        self.load_adjacents(source_file)
+    def __init__(self, stations_file, connections_file):
+        self.stations = self.load_stations(stations_file)
+        self.load_adjacents(connections_file)
 
         
-    def load_stations(self, source_file):
+    def load_stations(self, stations_file):
         """
         load all the stations into the graph
         """
         stations = {}
-        with open(source_file, 'r') as in_file:
+        with open(stations_file, 'r') as in_file:
             reader = csv.DictReader(in_file)
 
             for row in reader:
@@ -23,24 +23,25 @@ class Graph():
         return stations
 
 
-    # def load_adjacents(self, source_file):
-    #     """
-    #     load all the adjacents into the loaded stations
-    #     """
-    #     with open(source_file, 'r') as in_file:
-    #         reader = csv.DictReader(in_file)
+    def load_adjacents(self, connections_file):
+        """
+        load all the adjacents into the loaded stations
+        """
+        with open(connections_file, 'r') as in_file:
+            reader = csv.DictReader(in_file)
 
-    #         for row in reader:
-    #             adjacents = []
+            for row in reader:
+                row['station1'] = stations['station']
+                # adjacents = {}
 
-    #             for adjacent in row['adjacents'].split(','):
-    #                 # only add if the result is not an empty string
-    #                 if adjacent.strip('[] ') != '':
-    #                     adjacents.append(adjacent.strip('[] '))
+                # for adjacent in row['adjacents'].split(','):
+                #     # only add if the result is not an empty string
+                #     if adjacent.strip('[] ') != '':
+                #         adjacents.append(adjacent.strip('[] '))
 
-    #             station_id = row['id']
+                # station_id = row['id']
 
-    #             # add the adjacent to the correct node
-    #             for adjacent in adjacents:
-    #                 adjacent = self.stations[adjacent]
-    #                 self.stations[station_id].add_adjacent(adjacent)
+                # # add the adjacent to the correct node
+                # for adjacent in adjacents:
+                #     adjacent = self.stations[adjacent]
+                #     self.stations[station_id].add_adjacent(adjacent)
