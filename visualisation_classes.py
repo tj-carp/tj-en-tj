35a94ec52f719a8834fa2a5ac88356937c8b79f5
BillBelieve <billbelieveiot@gmail.com>
2023-01-17 15:24:40 +0100

    test station objecten

--- visualisation_classes.py
+++ visualisation_classes.py
@@ -14,23 +14,35 @@ if __name__ == "__main__":
 
     df = []
 
+    # maak dataframe van alle coordinaten van de stations
     for id, station in enumerate(stations):
         y_coord = float(stations[station].coords[0])
         x_coord = float(stations[station].coords[1])
-        # for k in stations[station].connections.keys():
-        #     print(type(k))
+        
         df.append([station, y_coord, x_coord, stations[station].connections.keys()])
 
+    # geef goeie kolom namen
     df = pd.DataFrame(df)
     df.columns = ['station', 'latitude', 'longitude', 'connections']
     print(df)
 
-    plt.scatter(df['longitude'], df['latitude'], s=10, c='yellow', edgecolors='blue', linewidths=1)
-    [plt.text( x=row['longitude'] + 0.005, y=row['latitude'], s=row['station'], fontsize=7) for k, row in df.iterrows()]
-    plt.xticks(rotation=90)
-    plt.show()
+    for id, row in enumerate(df.iterrows()):
+        connecties = []
+        for n in row[1]['connections']:
+            # print(n)
+            connecties.append(n)
+        # print(row[1]['station'])
+        # print(connecties)
 
+        for id_2, station in enumerate(connecties):
+            print(f"station: {id} {row[1]['station']}")
+            print(f"connectie: {id_2}")
+            print(stations[station])
 
+    # plt.scatter(df['longitude'], df['latitude'], s=10, c='yellow', edgecolors='blue', linewidths=1)
+    # [plt.text( x=row['longitude'] + 0.005, y=row['latitude'], s=row['station'], fontsize=7) for k, row in df.iterrows()]
+    # plt.xticks(rotation=90)
+    # plt.show()
 
 
 
