4fa71d4d7cbfaac1ab40a3654c950219b2bd4146
TJ <tessajleeuwerik@gmail.com>
2023-01-19 12:15:19 +0100

    cleanup main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from code.classes.graph import Graph
-from code.classes.railmap import RailMap
 from code.visualisation.visualisation import visualise
-from code.visualisation.visualise_graph import visualise_graph 
 from code.algorithms import randomise
 
 
@@ -13,12 +11,6 @@ if __name__ == "__main__":
 
     # read connections
     connections = Graph(stations_file, connections_file).connections
-
-    # use connections read to create random railmap
-    #random_railmap = randomise.create_railmap(connections)
-    #print(random_railmap)
-
-    #visualise(random_railmap, connections)
 
     scores = []
     railmaps = {}
