e228c6754fdb67e51ab96e6cd3767463395cd327
TJ <tessajleeuwerik@gmail.com>
2023-01-23 10:22:56 +0100

    no

--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -1,7 +1,9 @@
+from code.classes.railmap import RailMap
+
 def create_railmap(connections):
 
     stations = sum(set(map(lambda connection: (connection.station1, connection.station2), connections)),[])
 
     single_stations = tuple(filter(lambda station: stations.count(station) == 1,stations))
 
-    return single_stations
+    print(single_stations)
