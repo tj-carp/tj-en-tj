935d5bd445432c3da21a3085af2511e792a44c89
TJ <tessajleeuwerik@gmail.com>
2023-01-24 10:28:25 +0100

    very small changes

--- main.py
+++ main.py
@@ -30,13 +30,14 @@ if __name__ == "__main__":
     
     scores = []
     railmaps = {}
-    tries = 10000
+    tries = 1000
 
     for i in range(tries):
         random_railmap = randomise.create_railmap(connections)
         score = random_railmap.score()
         railmaps.update({score : random_railmap})
         scores.append(score)
+        print(i, score)
 
     visualise_scores(scores)
     scores.sort()
