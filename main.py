dd87617f8ff0bf95ff8f58528aa5109c6946edb9
BillBelieve <billbelieveiot@gmail.com>
2023-01-30 12:10:30 +0100

    changed visualise scores to accept the algoritm as an argument to save the output to the correct folder.
    updated README

--- main.py
+++ main.py
@@ -11,21 +11,14 @@ import numpy as np
 if __name__ == "__main__":
 
     # check command line arguments
-    if len(argv) not in [1]:
-        print("Usage: python3 main.py")
+    if len(argv) not in [1, 2]:
+        print("Usage: python3 main.py [area]")
         exit(1)
-    # request input to determine map name
-    print("For Holland, type 1 \nFor The Netherlands, type 2")
-    choice = input("Type here: ")
-
-    # choose input map name and if valid use to load data
-    if choice == '1':
+    # load the requested map or else holland
+    if len(argv) == 2:
+        map_name = argv[1]
+    elif len(argv) == 1:
         map_name = "holland"
-    elif choice == '2':
-        map_name = "national"
-    else:
-        print("you have bungled it")
-        exit(1)
 
     stations_file = f"data/stations_{map_name}.csv"
     connections_file = f"data/connections_{map_name}_text.csv"
@@ -36,14 +29,40 @@ if __name__ == "__main__":
 
     # --------------------------- Random ---------------------------------------
 
-    randomise.run(connections)
+    scores = []
+    railmaps = {}
+    tries = 1000
+
+    for i in range(tries):
+        random_railmap = randomise.create_railmap(connections)
+        score = random_railmap.score()
+        railmaps.update({score : random_railmap})
+        scores.append(score)
+
+
+    st_dev = np.std(scores)
+    scores.sort()
+
+    visualise_scores(scores, "randomise")
+    max_score = scores[(len(scores) - 1)]
+    min_score = scores[0]
+
+    result = f"\nAmount of runs: {tries} \n----------------------------------------------------------------\n"\
+             f"lowest score: {min_score}, highest score: {max_score}, average score: {round(sum(scores)/tries)}\n"\
+             f"----------------------------------------------------------------\n\n"\
+             f"{railmaps[max_score]}"
+    print(result)
+
+    visualise(railmaps[max_score], connections)
+    plt.show()
     
     # --------------------------- Greedy ---------------------------------------
 
-    # greedy_railmap = greedy.create_railmap(connections)
-    # score = greedy_railmap.score()
-    # visualise(greedy_railmap, connections)
+    greedy_railmap = greedy.create_railmap(connections)
+    score = greedy_railmap.score()
+    visualise(greedy_railmap, connections)
+    visualise_scores(score, "greedy")
 
    # --------------------------- Hillclimber ------------------------------------
 
-    #hillclimber.create_railmap(connections)
+    # hillclimber.create_railmap(connections)
