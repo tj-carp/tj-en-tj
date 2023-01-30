a42e1c1af7df606b239cf59199eaaf89c589553b
TJ <tessajleeuwerik@gmail.com>
2023-01-30 12:21:47 +0100

    main rewrite, randomise and hillclimber are now classes

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -1,62 +1,72 @@
 from code.classes.railmap import RailMap
 from code.visualisation.visualisation import visualise, visualise_scores
 import random
+from copy import deepcopy
 
-def create_railmap(connections):
-    # create empty railmap
-    railmap = RailMap(connections)
-    connection_ids = [*range(1, len(connections) + 1)]
-    # check whether holland or national map to determine maximum amount of routes in map
-    max_routes = 7 if len(connections) == 28 else 20
-    min_routes = 4 if len(connections) == 28 else 9
-    min_length = 100 if len(connections) == 28 else 160
+class Randomise():
+    def __init__(self, connections):
+        self.connections = connections
+        # create connections ids list
+        self.connection_ids = [*range(1, len(self.connections) + 1)]
+        # check whether holland or national map to determine maximum amount of routes in map
+        self.max_routes = 7 if len(self.connections) == 28 else 20
+        self.min_routes = 4 if len(self.connections) == 28 else 9
+        self.min_length = 100 if len(self.connections) == 28 else 160
+        # for best railmap
+        self.scores = []
+        self.railmaps = {}
+        self.tries = 100000 if len(self.connections) == 28 else 10000
 
+    def create_railmap(self):
+        # create empty railmap
+        railmap = RailMap(self.connections)
+        # fill railmap with 4 or 9 to 7 or 20 routes
+        while len(railmap.routes) < random.randint(self.min_routes, self.max_routes):
+            #create empty route
+            route = railmap.create_route()
 
-    # fill railmap with 4 or 9 to 7 or 20 routes
-    while len(railmap.routes) < random.randint(min_routes, max_routes):
-        #create empty route
-        route = railmap.create_route()
+            # create random route of anywhere between 100 or 160 and 120 or 180 minutes
+            while route.length < random.randint(self.min_length, route.max_length):
+                try:
+                    random_connection = random.choice(self.connection_ids)            
+                    route.add_connection(random_connection)
+                    self.connection_ids.remove(route.ids)
+                except:
+                    random_connection = random.randint(1, len(self.connections))            
+                    route.add_connection(random_connection)
+                
+            railmap.minutes += route.length
+            railmap.visited += route.ids
+            railmap.routes.append(route)
+        
+        return railmap
 
-        # create random route of anywhere between 100 or 160 and 120 or 180 minutes
-        while route.length < random.randint(min_length, route.max_length):
-            try:
-                random_connection = random.choice(connection_ids)            
-                route.add_connection(random_connection)
-                connection_ids.remove(route.ids)
-            except:
-                random_connection = random.randint(1, len(connections))            
-                route.add_connection(random_connection)
-            
-        railmap.minutes += route.length
-        railmap.visited += route.ids
-        railmap.routes.append(route)
-    
-    return railmap
-    
+    def create_best_railmap(self):
+        for i in range(self.tries):
+            random_railmap = create_railmap(self.connections)
+            score = random_railmap.score()
+            self.railmaps.update({score : random_railmap})
+            self.scores.append(score)
 
+        max_score = deepcopy(self.scores.sort()[(len(self.scores) - 1)])
 
-def run(connections):
-    scores = []
-    railmaps = {}
-    tries = 10000
+        best_random = deepcopy(self.railmaps[max_score])
 
-    for i in range(tries):
-        random_railmap = create_railmap(connections)
-        score = random_railmap.score()
-        railmaps.update({score : random_railmap})
-        scores.append(score)
+        return best_random
 
-    visualise_scores(scores)
-    scores.sort()
+    def run(self):
+        self.create_best_railmap()
+        
+        visualise_scores(self.scores)
+        self.scores.sort()
 
-    max_score = scores[(len(scores) - 1)]
-    min_score = scores[0]
+        max_score = self.scores.sort()[(len(self.scores) - 1)]
+        min_score = self.scores[0]
 
-    result = f"\nAmount of runs: {tries} \n----------------------------------------------------------------\n"\
-             f"lowest score: {min_score}, highest score: {max_score}, average score: {round(sum(scores)/tries)}\n"\
-             f"----------------------------------------------------------------\n\n"\
-             f"{railmaps[max_score]}"
-    
-    print(result)
-    visualise(railmaps[max_score], connections)
-    
+        result = f"\nAmount of runs: {self.tries} \n----------------------------------------------------------------\n"\
+                f"lowest score: {min_score}, highest score: {max_score}, average score: {round(sum(self.scores)/self.tries)}\n"\
+                f"----------------------------------------------------------------\n\n"\
+                f"{self.railmaps[max_score]}"
+        
+        print(result)
+        visualise(self.railmaps[max_score], self.connections)
--- main.py
+++ main.py
@@ -16,12 +16,12 @@ if __name__ == "__main__":
         exit(1)
     # request input to determine map name
     print("For Holland, type 1 \nFor The Netherlands, type 2")
-    choice = input("Type here: ")
+    map_choice = input("Type here: ")
 
     # choose input map name and if valid use to load data
-    if choice == '1':
+    if map_choice == '1':
         map_name = "holland"
-    elif choice == '2':
+    elif map_choice == '2':
         map_name = "national"
     else:
         print("you have bungled it")
@@ -33,17 +33,25 @@ if __name__ == "__main__":
     # read connections from given data
     connections = Graph(stations_file, connections_file).connections
 
+    # request input to determine which algorithm to run
+    print("Which algorithm do you want to run?\nFor randomise, type 1\nFor greedy, type 2\nFor hillclimber, type 3\nTo run all algorithms, type 4 "\
+        "[Please note that this option will take at least 3 hours to run] ")
+    alg_choice = input("Type here: ")
 
-    # --------------------------- Random ---------------------------------------
+    if alg_choice == '1' or '4':
+        randomise = randomise.Randomise(connections)
+        randomise.run()
+    if alg_choice == '2' or '4':
+        greedy_railmap = greedy.create_railmap(connections)
+        score = greedy_railmap.score()
+        visualise(greedy_railmap, connections)
+    if alg_choice == '3' or '4':
+        print("Type 1 to start hillclimber off with a random railmap, or type 2 to run randomise first and start off with the best random railmap")
+        start_choice = input("Type here: ")
+        hillclimber = hillclimber.HillClimber(connections, start_choice)
+        hillclimber.create_railmap()
+    else:
+        print("you have bungled it")
+        exit(1)
 
-    randomise.run(connections)
     
-    # --------------------------- Greedy ---------------------------------------
-
-    # greedy_railmap = greedy.create_railmap(connections)
-    # score = greedy_railmap.score()
-    # visualise(greedy_railmap, connections)
-
-   # --------------------------- Hillclimber ------------------------------------
-
-    #hillclimber.create_railmap(connections)
