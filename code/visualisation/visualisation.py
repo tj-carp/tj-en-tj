c2e776c8661ac20f6041eb47d01bb301358e25eb
BillBelieve <billbelieveiot@gmail.com>
2023-01-31 22:12:25 +0100

    fixed malfunctioning railmap printer

--- code/visualisation/visualisation.py
+++ code/visualisation/visualisation.py
@@ -5,14 +5,7 @@ from datetime import datetime
 import os
 
 def visualise(railmap, connections, output_folder):
-    """
-    creates a map of the netherlands
-    Reads all connections given as an argument
-    Plots the connections on the map
-    takes routes from railmap and plots them to visualize the trajectories
-    """
 
-    # creates the map object 
     map = Basemap(projection='merc',
                     llcrnrlat=50.5,
                     urcrnrlat=53.5,
@@ -27,12 +20,13 @@ def visualise(railmap, connections, output_folder):
     map.drawparallels(np.arange(-90, 90, 30))
     map.plot([53, 54, 55, 56], [3,4,5,6], color="r")
 
-    # maps the connections
-    x_route_map = []
-    y_route_map = []
+    x_coords_map = []
+    y_coords_map = []
 
     for connection in connections:
         connection = connections[connection]
+        x_route_map = []
+        y_route_map = []
         x_route_map.append(connection.station1.coords[1])
         y_route_map.append(connection.station1.coords[0])
         x_route_map.append(connection.station2.coords[1])
@@ -43,7 +37,9 @@ def visualise(railmap, connections, output_folder):
         map.plot(x, y, color='grey', linestyle='dashed', linewidth=1)
         map.scatter(x, y)
 
-    # maps the routes
+    x_coords = []
+    y_coords = []
+
     for route in railmap.routes:
         x_route = []
         y_route = []
@@ -55,10 +51,9 @@ def visualise(railmap, connections, output_folder):
         x, y = map(x_route, y_route)
         map.plot(x, y, '-o')
 
-    # visualise and save output
     now = str(datetime.now())
-    print(f"saved file as railmap")
-    map.scatter(x_route_map, y_route_map)
+    print(f"saved file as railmap-{now}")
+    map.scatter(x_coords, y_coords)
     plt.savefig(f"{output_folder}/railmap.png")
     plt.show()
 
